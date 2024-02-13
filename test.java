cca2ebcdcea77e06315d59e4a3be35fe8bfbc592
SanginiMiharia <500094896@stu.upes.ac.in>
2024-02-13 21:22:01 +0530

    Second Commit

--- test.java
+++ test.java
@@ -3,10 +3,14 @@ public class test{
 	{
 		return(a+b);
 	}
-	
+	public int diff(int c , int d)
+    {
+        return(c-d);
+    }
 	public static void main(String[] args) {
 		
 	test ob= new test();
 		System.out.println(ob.sum(10,2));
+        System.out.println(ob.diff(10,2));
 		}
 	}
