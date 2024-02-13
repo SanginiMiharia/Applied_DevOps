8f1d6a95257b59bbf48b3922cd55a9c23c693222
SanginiMiharia <500094896@stu.upes.ac.in>
2024-02-13 21:19:29 +0530

    First Commit

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test{
+	public int sum(int a , int b)
+	{
+		return(a+b);
+	}
+	
+	public static void main(String[] args) {
+		
+	test ob= new test();
+		System.out.println(ob.sum(10,2));
+		}
+	}
